bed2c7ac6dfa5a23751eb024ca4e9e0c60d5d5ae
Michael J. Simons <michael.simons@neo4j.com>
2018-10-04 13:18:33 +0200

    Fix checkstyle errors.

--- examples/src/main/java/org/neo4j/reactiveclient/examples/HelloWorldExampleUsingRxJava2.java
+++ examples/src/main/java/org/neo4j/reactiveclient/examples/HelloWorldExampleUsingRxJava2.java
@@ -15,13 +15,13 @@
  */
 package org.neo4j.reactiveclient.examples;
 
-import io.reactivex.Flowable;
-import io.reactivex.Single;
-
 import org.neo4j.reactiveclient.Neo4jClients;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.bridge.SLF4JBridgeHandler;
+
+import io.reactivex.Flowable;
+import io.reactivex.Single;
 
 /**
  * Simple Hello World.
