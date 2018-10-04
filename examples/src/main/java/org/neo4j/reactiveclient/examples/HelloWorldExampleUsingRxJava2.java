4dd179aa0c7edc4f3ef3e948f7a585ea4a27e57b
Michael J. Simons <michael.simons@neo4j.com>
2018-10-04 13:02:44 +0200

    Add RxJava2 example.

---
+++ examples/src/main/java/org/neo4j/reactiveclient/examples/HelloWorldExampleUsingRxJava2.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2018 "Neo4j Sweden AB" <https://neo4j.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.neo4j.reactiveclient.examples;
+
+import io.reactivex.Flowable;
+import io.reactivex.Single;
+
+import org.neo4j.reactiveclient.Neo4jClients;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.bridge.SLF4JBridgeHandler;
+
+/**
+ * Simple Hello World.
+ *
+ * @author Michael J. Simons
+ */
+public class HelloWorldExampleUsingRxJava2 {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(HelloWorldExampleUsingRxJava2.class);
+
+	public static void main(final String... args) {
+		SLF4JBridgeHandler.removeHandlersForRootLogger();
+		SLF4JBridgeHandler.install();
+
+		var client = Neo4jClients.create("bolt://localhost:7687", "neo4j", "music");
+
+		Flowable.fromPublisher(client.execute("MATCH (n) RETURN n"))
+			.take(5)
+			.map(r -> r.get("n").asNode().labels().toString())
+			.doOnNext(r -> LOGGER.info(r))
+			.toList()
+			.flatMap(ignored -> Single.fromPublisher(client.close()))
+			.doOnSuccess(aVoid -> LOGGER.info("Client was closed."))
+			.subscribe();
+	}
+}
